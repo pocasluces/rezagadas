a794d90ead06e626bee59d547a3e939424c2474e
carlos <carlos.gonzalez@openmailbox.org>
2015-09-15 10:44:54 +0200

    añadiendo usuario sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => ENV['pocas@luces'],
+  :password       => ENV['miedo@oscuridad3'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
