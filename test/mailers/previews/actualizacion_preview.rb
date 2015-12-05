40f122308b8878d8b974d3c980f1974c287cebba
carlos <carlos.gonzalez@openmailbox.org>
2015-12-05 21:47:15 +0100

    Envio de correo con enlace directo al post

--- test/mailers/previews/actualizacion_preview.rb
+++ test/mailers/previews/actualizacion_preview.rb
@@ -3,7 +3,8 @@ class ActualizacionPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/actualizacion/notificacion
   def notificacion
-    post= Post.new(titulo:'las galaxias lejanas')
+
+    post= EntradaBlog.last
     usuario = Usuario.first
     Actualizacion.notificacion(post, usuario)
   end
